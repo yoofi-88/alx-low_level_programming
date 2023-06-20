64aba4e7e29e7604495b5b0625a92af4f0578eea
yoofi-88 <charlesanthonywilliams2@gmail.com>
2023-06-20 13:42:34 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
+
 	/**
-	 * main - a program that prints _putchar, followed by a new line
+	 * main -A program that prints _putchar, followed by a new line
 	 * Return: Always 0
 	 */
 
