3eeaee25e549fa4b1b01ed2b794fd09b549adc97
yoofi-88 <charlesanthonywilliams2@gmail.com>
2023-06-20 14:29:28 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,7 @@
 
 
 	/**
-	 * main - Entry Point
-	 * Description: 'A program that prints _putchar, followed by a new line'
-	 * @parameter: describe the parameter
+	 * main - Entry point
 	 * Return: Always 0
 	 */
 
