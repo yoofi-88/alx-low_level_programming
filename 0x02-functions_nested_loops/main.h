d4dd25dfbbccfdf458f78208b41421f94010c6b8
yoofi-88 <charlesanthonywilliams2@gmail.com>
2023-06-20 14:18:34 -0700

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef main_h
 	#define main_h
 
-
-	int _putchar(char);
+	int main(void);
+	int _putchar(char);	
 	void print_alphabet(void);
 	void print_alphabet_x10(void);
 	int _islower(int c);
