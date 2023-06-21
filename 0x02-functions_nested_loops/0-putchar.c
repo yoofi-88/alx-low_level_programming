3e27462cbf1b7bd251b2ccd25a74761960c74459
yoofi-88 <charlesanthonywilliams2@gmail.com>
2023-06-21 04:13:23 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 
-	/**
-	 * main - Entry point
-	 * Return: Always 0
-	 */
-
-
-	int main(void)
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
 
 	{
 	_putchar('_');
