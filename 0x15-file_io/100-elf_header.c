3f6ab2247c0d79ea8c844ea617cc5a3f504f90f9
Charles <charlesanthonywilliams2@gmail.com>
2023-10-03 12:04:31 -0700

    100-elf_header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -180,8 +180,10 @@ void print_osabi(unsigned char *e_ident)
  * print_abi - Prints the ABI version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF ABI version.
  */
-void print_abi(unsigned char *e_ident) {
-	printf("ABI Version:%d\n", e_ident[EI_ABIVERSION]);
+void print_abi(unsigned char *e_ident)
+{
+	printf("  ABI Version:                       %d\n",
+	       e_ident[EI_ABIVERSION]);
 }
 
 /**
